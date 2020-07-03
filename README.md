b04f9f98b93fcac82ba4a6a5799249d4f89f58fd
nbagheri <negar_bagheri_hariri@sfu.ca>
2020-07-03 11:56:22 -0700

    docker worked

---
+++ README.md
@@ -0,0 +1,4 @@
+
+my project can be found on  localhost:8080/polls and localhost:8080/admin
+admin username : negar 
+password : shermin 
--- ex7/tutorial-env/bin/django-admin.py
+++
@@ -1,7 +0,0 @@
-#!/Users/negarhariri/Desktop/ex7/tutorial-env/bin/python
-# EASY-INSTALL-DEV-SCRIPT: 'Django==3.2','django-admin.py'
-__requires__ = 'Django==3.2'
-__import__('pkg_resources').require('Django==3.2')
-__file__ = '/Users/negarhariri/Desktop/ex7/django/django/bin/django-admin.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
